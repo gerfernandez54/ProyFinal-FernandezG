82985f753bb4892de1f7da2c7c0b8ca7c788e253
gerfernandez54 <germanfer54@gmail.com>
2022-07-20 20:32:10 -0300

    Initial commit

---
+++ Academico/urls.py
@@ -0,0 +1,12 @@
+from django.urls import URLPattern, path
+from Academico import views
+
+urlpatterns = [
+    path('', views.home),
+    path('registrarCurso/', views.registrarCurso),
+    path('edicionCurso/<codigo>', views.edicionCurso),
+    path('editarCurso/', views.editarCurso),
+    path('eliminarCurso/<codigo>', views.eliminarCurso)
+
+
+]
---
+++ Universidad/readme.txt
@@ -0,0 +1,4 @@
+Entrega1 Fernandez German
+Pre entrega de trabajo practico 
+
+Debido a que los otros dos participantes no respondieron a ninguna comunicacion el TP lo realice solo. 
